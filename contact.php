b86170f767bab802f10ac079b2f80138d8a803aa
Aman Mishra <mishrakumaraman@gmail.com>
2016-09-17 21:50:44 +0530

    first commit on 17 sep 2016

---
+++ contact.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Us</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Contact Us</u></h3>
+            
+            <div class="contact">
+            <h3 style="color:#2E4372;"><u>Kolkata Branch</u></h3>
+            <p><span class="heading">Address - </span>Online Banking Company, IBRAD buisness school, Keshtopur, Kolkata.</p>
+            <p><span class="heading">Tel - </span>033-456892/12</p>
+            <p><span class="heading">Email - </span>kolkatabranch@onlinebank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Delhi Branch</u></h3>
+            <p><span class="heading">Address - </span>Online Banking Company, Sector V-A , Malviya Nagar, Delhi.</p>
+            <p><span class="heading">Tel - </span>011-456856/32</p>
+            <p><span class="heading">Email - </span>delhibranch@onlinebank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Bangalore Branch</u></h3>
+            <p><span class="heading">Address - </span>Online Banking Company, Near City Center, Kamarthalli, Bangalore.</p>
+            <p><span class="heading">Tel - </span>022-456854/11</p>
+            <p><span class="heading">Email - </span>bangalorebranch@onlinebank.com</p>
+
+			
+			
+            </div>
+            </div>
